34b27c2ee491df5eebf700e48d4855579859715d
sambajam <sambajam@gmail.com>
2025-02-26 20:03:11 +0100

    Version 3

--- CryptoAPI/obj/Debug/net9.0/CryptoAPI.AssemblyInfo.cs
+++ CryptoAPI/obj/Debug/net9.0/CryptoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b363102b9f265bda754763cace8d06977a60b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
